a9b46e694dca39924d221b5d753b2f0a30f84fd0
Mohamad <M.Beheshti148@gmail.com>
2026-06-09 18:21:26 +0330

    fix bug of filters

--- main.py
+++ main.py
@@ -12,19 +12,23 @@ app = Client(
     bot_token=BOT_TOKEN
 )
 
-@app.on_message(filters.command('start'), filters.user(ADMINS))
+os.makedirs("downloads", exist_ok=True)
+
+@app.on_message(filters.command('start') & filters.user(ADMINS))
 async def start_handler(client: Client, message: Message):
     global ADMIN_DATA
     chat_id = message.chat.id
 
-    if chat_id in ADMIN_DATA:
-        set_admin_data(chat_id)
-        await message.reply_text('☀️ ویدیو رو بفرست تا به ویدیو مسیج تبدیلش کنم\n.', quote=True)
+    set_admin_data(chat_id)
 
-@app.on_message(filters.video, filters.user(ADMINS))
+    await message.reply_text('☀️ ویدیو رو بفرست تا به ویدیو مسیج تبدیلش کنم\n.', quote=True)
+
+@app.on_message(filters.video & filters.user(ADMINS))
 async def convert_handler(client: Client, message: Message):
     global ADMIN_DATA
     chat_id = message.chat.id
+
+    set_admin_data(chat_id)
 
     await client.send_video(chat_id=chat_id, video=message.video.file_id, reply_markup=convert_button)
 
@@ -35,34 +39,38 @@ async def call(client: Client, call: CallbackQuery):
     msg_id = call.message.id
     chat_id = call.message.chat.id
 
+    set_admin_data(chat_id)
+
     if data == "convert_to_video_message" and call.message.video:
         if call.message.video.duration > 60:
             await call.answer("زمان ویدیو بیشتر از یک دقیقه است ❌", show_alert=False)
             return
 
-        if ADMINS[chat_id]["in_queue"] == True:
+        if ADMIN_DATA[chat_id]["in_queue"] == True:
             await call.answer(
                 "شما از قبل یک ویدیو در صف تبدیل ویدیو به ویدیو مسیج دارید, لطفا تا پایان عملیات قبلی منتظر بمانید ❣️",
                 show_alert=False)
             return
 
         await call.answer("ویدیو درحال دانلود است لطفا صبر کنید ✅", show_alert=False)
-        ADMINS[chat_id]["in_queue"] = True
+        ADMIN_DATA[chat_id]["in_queue"] = True
+        file_path = None
+        try:
+            file_path = await client.download_media(
+                call.message,
+                file_name=f"downloads/{uuid4()}.mp4",
+            )
 
-        file_path = await client.download_media(
-            call.message,
-            file_name=f"downloads/{uuid4()}.mp4",
-        )
+            video_note = await client.send_video_note(chat_id=chat_id, video_note=file_path)
 
-        video_note = await client.send_video_note(chat_id=chat_id, video_note=file_path)
+            await client.send_message(chat_id=chat_id,
+                                      text="ویدیو با موفقیت به ویدیو مسیج تبدیل شد ✅\n.",
+                                      reply_to_message_id=video_note.id)
+        finally:
+            ADMIN_DATA[chat_id]["in_queue"] = False
 
-        await client.send_message(chat_id=chat_id,
-                                  text="ویدیو با موفقیت به ویدیو مسیج تبدیل شد ✅\n.",
-                                  reply_to_message_id=video_note.id)
-        ADMINS[chat_id]["in_queue"] = False
-
-        if os.path.exists(file_path):
-            os.remove(file_path)
+            if os.path.exists(file_path):
+                os.remove(file_path)
 
     elif data == "cancel":
         await client.delete_messages(chat_id, msg_id)
